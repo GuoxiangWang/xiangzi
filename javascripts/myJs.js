76ce9b34f9395b989fc7c701ab5cce3d2a59eba3
GuoxiangWang <wanggxbupt@163.com>
2016-06-11 16:58:46 +0800

    6月11最终版

--- javascripts/myJs.js
+++ javascripts/myJs.js
@@ -5,7 +5,7 @@ $(function(){
 <!--ÒôÀÖ¿ØÖÆÆ÷µÄÉ¾³ýÓëÌí¼Ó-->
 $(".icon-music").click(function()
 {
-if(document.getElementById("musicControl").childNodes.length==5)
+if(document.getElementById("musicControl").childNodes.length==4)
 {$(this).next().remove();}
 else{$(this).after("<audio src='media/bg.mp3' controls='controls' autoplay='autoplay'></audio>")};
 })
