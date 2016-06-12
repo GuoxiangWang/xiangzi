536267ff2ff75bddb973d4875589139e0e51346d
GuoxiangWang <wanggxbupt@163.com>
2016-06-12 18:30:42 +0800

    修改侧边栏2

--- javascripts/myJs.js
+++ javascripts/myJs.js
@@ -7,7 +7,7 @@ $(".icon-music").click(function()
 {
 if(document.getElementById("musicControl").childNodes.length==4)
 {$(this).next().remove();}
-else{$(this).after("<audio src='media/bg.mp3' controls='controls' autoplay='autoplay'></audio>")};
+else{$(this).after("<audio src='https://raw.githubusercontent.com/GuoxiangWang/xiangzi/gh-pages/media/bg.mp3' controls='controls' autoplay='autoplay'></audio>")};
 })
 
 
