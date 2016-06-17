5886916ba372edc0cd29e78e0d47a78827e725c0
GuoxiangWang <wanggxbupt@163.com>
2016-06-17 10:19:06 +0800

    添加音乐控制器动画

--- javascripts/myJs.js
+++ javascripts/myJs.js
@@ -10,7 +10,15 @@ if(document.getElementById("musicControl").childNodes.length==4)
 else{$(this).after("<audio src='https://raw.githubusercontent.com/GuoxiangWang/xiangzi/gh-pages/media/bg.mp3' controls='controls' autoplay='autoplay'></audio>")};
 })
 
+<!--ÒôÀÖ¿ØÖÆÆ÷µÄÉÁË¸¶¯»­-->
 
+
+
+var int=self.setInterval("twinkle()",618)
+
+
+
+<!--Èý½£¿ÍÍ¼ÏñÊó±êÐü¸¡¶¯»­-->
 $("#learning .thumbnail").mouseover(function(){
 $(this).find("img").css({"height":"10em"});
 }).mouseout(function(){
@@ -19,3 +27,14 @@ $(this).find("img").css({"height":"5em"});
 
 }
 )
+
+
+function twinkle(){
+	if($("#musicControl").css("visibility")=="visible")
+	{
+		$("#musicControl").css("visibility","hidden");
+	}
+	else
+		$("#musicControl").css("visibility","visible");
+	
+}
